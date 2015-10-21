8bf0e921ff55eafa5ba19290cdcad1bcb7fc3c2c
cz <598799959@qq.com>
2015-10-21 14:05:53 +0800

    add keepThread

--- src/main/java/edu/thu/ebgp/controller/LinkKeepThread.java
+++ src/main/java/edu/thu/ebgp/controller/LinkKeepThread.java
@@ -1,9 +1,20 @@
 package edu.thu.ebgp.controller;
 
+import java.util.HashMap;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+
 import org.projectfloodlight.openflow.protocol.OFMessage;
+import org.projectfloodlight.openflow.protocol.OFPacketIn;
 import org.projectfloodlight.openflow.protocol.OFPortDesc;
 import org.projectfloodlight.openflow.protocol.OFType;
+import org.projectfloodlight.openflow.protocol.OFVersion;
+import org.projectfloodlight.openflow.protocol.match.MatchField;
 import org.projectfloodlight.openflow.types.DatapathId;
+import org.projectfloodlight.openflow.types.EthType;
+import org.projectfloodlight.openflow.types.IpProtocol;
+import org.projectfloodlight.openflow.types.OFPort;
+import org.projectfloodlight.openflow.types.TransportPort;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -15,6 +26,11 @@ import net.floodlightcontroller.core.IOFSwitchListener;
 import net.floodlightcontroller.core.PortChangeType;
 import net.floodlightcontroller.core.internal.IOFSwitchService;
 import net.floodlightcontroller.core.module.FloodlightModuleContext;
+import net.floodlightcontroller.core.util.SingletonTask;
+import net.floodlightcontroller.packet.Data;
+import net.floodlightcontroller.packet.Ethernet;
+import net.floodlightcontroller.packet.IPv4;
+import net.floodlightcontroller.packet.UDP;
 import net.floodlightcontroller.threadpool.IThreadPoolService;
 
 public class LinkKeepThread implements IOFMessageListener,IOFSwitchListener{
@@ -23,6 +39,8 @@ public class LinkKeepThread implements IOFMessageListener,IOFSwitchListener{
 	private IFloodlightProviderService floodlightProvider;
 	private IOFSwitchService switchService;
 	private IThreadPoolService threadPool;
+	private SingletonTask keepTask;
+	private HashMap<String, Long> timermap = new HashMap<String, Long>();
 
 	public LinkKeepThread(FloodlightModuleContext context){
 		threadPool = context.getServiceImpl(IThreadPoolService.class);
@@ -32,6 +50,19 @@ public class LinkKeepThread implements IOFMessageListener,IOFSwitchListener{
 	public void start(){
 		floodlightProvider.addOFMessageListener(OFType.PACKET_IN, this);
 		switchService.addOFSwitchListener(this);
+		ScheduledExecutorService ses = threadPool.getScheduledExecutor();
+		keepTask = new SingletonTask(ses, new Runnable() {
+			@Override
+			public void run() {
+				try {
+					//discoverLinks();
+				} catch (Exception e) {
+					logger.error("Exception in keepAlive.", e);
+				} finally {
+					keepTask.reschedule(30, TimeUnit.SECONDS);
+				}
+			}
+		});
 	}
 	@Override
 	public String getName() {
@@ -52,8 +83,41 @@ public class LinkKeepThread implements IOFMessageListener,IOFSwitchListener{
 	@Override
 	public net.floodlightcontroller.core.IListener.Command receive(
 			IOFSwitch sw, OFMessage msg, FloodlightContext cntx) {
-		// TODO Auto-generated method stub
-		return null;
+
+		switch (msg.getType()) {
+        	case PACKET_IN:
+        		OFPacketIn myPacketIn = (OFPacketIn) msg;
+        		OFPort myInPort = (myPacketIn.getVersion().compareTo(OFVersion.OF_12) < 0) 
+        		? myPacketIn.getInPort() : myPacketIn.getMatch().get(MatchField.IN_PORT);
+        		Ethernet eth = IFloodlightProviderService.bcStore.get(cntx,
+        				IFloodlightProviderService.CONTEXT_PI_PAYLOAD);
+        		//logger.info("Packet in: inport: {} seen on switch: {}", myInPort.toString(), sw.getId().toString());
+        		//logger.info("Packet in: srcmac: {}, dstmac: {}",
+        		//		eth.getSourceMACAddress().toString(), eth.getDestinationMACAddress().toString());
+        		if (eth.getEtherType() == EthType.IPv4){
+        			IPv4 ipv4 = (IPv4) eth.getPayload();
+        			if (ipv4.getProtocol() == IpProtocol.UDP){
+        				UDP udp = (UDP) ipv4.getPayload();
+        				TransportPort srcPort = udp.getSourcePort();
+        				TransportPort dstPort = udp.getDestinationPort();
+        				if (srcPort.equals(TransportPort.of(30001)) && dstPort.equals(TransportPort.of(30002))){
+        					Data data = (Data) udp.getPayload();
+            				byte[] databyte = data.getData();
+            				String datastring = new String(databyte);
+            				//String infostr = String.format("Packet in: inport: %s seen on switch: %s, " +
+            				//		"SrcPort: %s, DstPort: %s, Payload: %s", myInPort.toString(), sw.getId().toString(), 
+            				//		srcPort.toString(), dstPort.toString(), datastring);
+            				//logger.info(infostr);
+            				String switchport = sw.getId().toString() + ": " + myInPort.toString();
+            				timermap.put(switchport, System.currentTimeMillis());
+        				}
+        			}
+        		}
+        		break;
+            default:
+                break;
+		}
+		return Command.CONTINUE;
 	}
 	///////////////////////
 	// IOFSwitchListener //
